a538403ddd2c1f9e48bb8bb84c67ac693c6baa32
Toni Merhar <merha013@umn.edu>
2024-02-19 14:22:17 -0600

    Initial Commit

---
+++ R/week6.R
@@ -0,0 +1,8 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+# line reserved for library call
+# line reserved for library call
+# line reserved for library call
+
+# Data Import
