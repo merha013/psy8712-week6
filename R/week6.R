1affe2f7d9b40be6ab93f93e0190b1aa270a4c52
Toni Merhar <merha013@umn.edu>
2024-02-21 18:31:12 -0600

    Still working on data import.

--- R/week6.R
+++ R/week6.R
@@ -1,8 +1,28 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-# line reserved for library call
-# line reserved for library call
+library(stringi)
+library(rebus)
 # line reserved for library call
 
 # Data Import
+citations <- stri_read_lines("../data/citations.txt", encoding = '297')
+citations_txt <- stri_trim_both(citations, pattern = "[:WHITE_SPACE:]")
+# stri_trim_both(citations, pattern = "[:space:]") <-- this works.?
+# stri_trim_both(citations, pattern = "^[:space:]*$")
+# citations[stri_trim_both(citations) != ""] 
+# stri_trim_both(citations, pattern != "", negate = TRUE) 
+# stri_trim(citations, side = "both")
+sum(str_count(citations) - str_count(citations_txt))
+#sum(str_length(citations)-str_length(citations_txt))
+mean(nchar(citations_txt))
+  
+# Data Cleaning
+citations_tbl %>%
+  sample_n(20)
+#next row should be on line 17
+citations_tbl <- 
+  
+  
+# practice area
+writeLines(citations)
