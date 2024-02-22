c4c849043ac27044be9f8cf6a0cdb4ced165721f
Toni Merhar <merha013@umn.edu>
2024-02-22 16:35:02 -0600

    Done through line 20.

--- R/week6.R
+++ R/week6.R
@@ -3,7 +3,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(stringi)
 library(rebus)
-# line reserved for library call
+# library(dplyr)
 
 # Data Import
 citations <- stri_read_lines("../data/citations.txt", encoding = "CP1252")
@@ -15,10 +15,16 @@ mean(nchar(citations_txt))
 citations_tbl %>%
   sample_n(20)
 citations_tbl <- tibble(line = seq_along(citations_txt), cite = citations_txt) %>%
-  str_sub(start = '|\\", end = '|\\") #still need to work on this one.
+  # str_replace_all(coll("\"'"), "")    # str_replace_all("[\"']", "")
+  mutate(year = (str_match(string = citations_tbl$cite, pattern = "(([1-2][0-9]{3}))")[,1])) %>% 
+  mutate(citations_tbl, page_start = (str_match(string = citations_tbl$cite, pattern = "\\b(\\d+)(?=-)")[,1])) 
 
-  
+mutate(perf_ref = )
+
+
 # practice area
 writeLines(citations)
 writeLines(citations_txt)
 View(citations_tbl)
+is.atomic(citations_tbl)
+class(citations_tbl)
