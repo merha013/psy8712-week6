02f8cf1924a5f0f85ca2e22e4d5a59edfce7f7f3
Toni Merhar <merha013@umn.edu>
2024-02-21 22:22:44 -0600

    Still working on line 18.

--- R/week6.R
+++ R/week6.R
@@ -8,17 +8,17 @@ library(rebus)
 # Data Import
 citations <- stri_read_lines("../data/citations.txt", encoding = "CP1252")
 citations_txt <- citations[stri_trim_both(citations) != ""] 
-sum(str_count(citations) - str_count(citations_txt)) 
 sum(length(citations)-length(citations_txt))
 mean(nchar(citations_txt))
   
-# Data Cleaning <--line 14
+# Data Cleaning
 citations_tbl %>%
   sample_n(20)
-citations_tbl <- 
+citations_tbl <- tibble(line = seq_along(citations_txt), cite = citations_txt) %>%
+  str_sub(start = '|\\", end = '|\\") #still need to work on this one.
 
   
 # practice area
 writeLines(citations)
 writeLines(citations_txt)
-str_view(citations, pattern = START %R% "C")
+View(citations_tbl)
