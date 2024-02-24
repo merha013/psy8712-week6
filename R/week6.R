707a908b1c86fb16b719995e60eb8657ed990b9a
Toni Merhar <merha013@umn.edu>
2024-02-23 23:35:47 -0600

    Still working on lines 18 and 21.

--- R/week6.R
+++ R/week6.R
@@ -3,7 +3,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(stringi)
 library(rebus)
-# library(dplyr)
+library(dplyr)
 
 # Data Import
 citations <- stri_read_lines("../data/citations.txt", encoding = "CP1252")
@@ -15,19 +15,24 @@ mean(nchar(citations_txt))
 citations_tbl %>%
   sample_n(20)
 citations_tbl <- tibble(line = seq_along(citations_txt), cite = citations_txt) %>%
-  # str_replace_all(coll("\"'"), "")    # str_replace_all("[\"']", "")
+# input line 18
   mutate(year = (str_match(string = cite, pattern = "(([1-2][0-9]{3}))")[,1])) %>%
   mutate(page_start = (str_match(string = cite, pattern = "\\b(\\d+)(?=-)")[,1])) %>%
-  mutate(perf_ref = str_detect(string = cite, pattern = "performance")) # need to ensure this is capturing capitalization as well
+  mutate(perf_ref = str_detect(string = cite, pattern = "performance")) %>% # need to ensure this is capturing capitalization as well
+  mutate(title = (str_match(string = cite, pattern = "\\). (.+?)[.!?]")[,2])) %>%
+  mutate(first_author = (str_match(string = cite, pattern ="(([A-Z][a-z]+,\\s[A-Z]\\.)(\\s[A-Z]\\.)?)")[,1]))
+
+sum(!is.na(citations_tbl$first_author))
 
 
+#practice area for line 18
+mutate(cite = str_replace_all(string = citations_tbl$cite, pattern = "[\"|']", replacement = ""))
+mutate(cite = str_replace_all(coll(string = citations_tbl$cite, pattern = "[\"|']", replacement = ""))
+mutate(str_replace_all(string = citations_tbl$cite, pattern = "[\"\']", "")
+str_replace_all(citations_tbl$cite, pattern = "[\"']", replacement = "")
 
-# line 21 option: str_detect(cite, regex("performance", ignore_case = TRUE))
 
-
-# practice area
-writeLines(citations)
-writeLines(citations_txt)
-View(citations_tbl)
-is.atomic(citations_tbl)
-class(citations_tbl)
+# practice area for line 21
+mutate(perf_ref = str_detect(string = citations_tbl$cite, pattern = regex("performance", negate = TRUE))
+       mutate(perf_ref = str_detect(string = citations_tbl$cite, pattern = regex("performance", ignore_case = TRUE))
+              
